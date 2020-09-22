ec49939ec77a33d181531d6f9c595933675d4510
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2020-09-22 17:39:16 -0300

    second

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ function getInputs() {
     var test = document.getElementById("principal").value;
 
     var dict = {
-        pv: document.getElementById("principal").value,
-        r: document.getElementById("taxaJuros").value,
-        n: document.getElementById("periodos").value
+        pv: Number(document.getElementById("principal").value),
+        r: Number(document.getElementById("taxaJuros").value),
+        n: Number(document.getElementById("periodos").value)
     };
 
     return dict
 }
 
-function test() {
-    console.log("testestestes");
-}
-
 function simpleInterest() {
     tempValues = getInputs();
-    document.getElementById("montanteSimples").innerHTML = `O montante é ${Math.round(tempValues.pv * (1 + tempValues.r * tempValues.n))}`;
-    console.log(`O montante é ${Math.round(tempValues.pv * (1 + tempValues.r * tempValues.n))}`)
+    document.getElementById("montanteSimples").innerHTML = `O montante a juros simples é ${(tempValues.pv * (1 + tempValues.r * tempValues.n)).toFixed(2)}`;
+}
+
+function compoundInterest() {
+    tempValues = getInputs();
+    document.getElementById("montanteComposto").innerHTML = `O montante a juros compostos é ${(tempValues.pv * Math.pow((1 + tempValues.r), tempValues.n)).toFixed(2)}`;
+}
+
+function continuousInterest() {
+    tempValues = getInputs();
+    document.getElementById("montanteContinuo").innerHTML = `O montante a juros contínuos é ${(tempValues.pv * Math.exp(tempValues.r * tempValues.n)).toFixed(2)}`;
 }
