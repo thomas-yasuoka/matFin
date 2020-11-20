7512fb93e75de1f2befc0dac4453f7b1aa2e4f74
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2020-11-20 15:00:21 -0300

    falta optim

--- index.js
+++ index.js
@@ -51,74 +51,102 @@ function regularSeries() {
     document.getElementById("serieUniformePostecipada").innerHTML = `O montante a partir da série uniforme postecipada é ${calc.toFixed(2)}`;
 }
 
+// A funcionalidade de fato
+
+
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
     };
     xhr.send();
 };
 
-function get_selic() {
-    let url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json';
+function get_cdi() {
+    let url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4391/dados?formato=json';
     var prom;
-
+    
     prom = fetch(url)
     .then(res => res.json())
     .then((out) => {
-    // a = out;
+        // a = out;
     // console.log('Checkout this JSON! ', out);
     return out
-    })
-    .catch(err => { throw err });
-    return prom
+})
+.catch(err => { throw err });
+return prom
 };
 
-function working_with_selic(){
-    prom = get_selic()
-    prom.then(function(result){
-        // you work the results here
-        console.log(result.slice(-2)[0])
-    })
+async function cdi(){
+    prom = get_cdi();
+    let x = await prom.then(result => result);
+    
+    // pega os últimos doze meses
+    x = x.slice(-13, -1);
+    console.log(x);
+    
+    return x
 }
 
-function test() {
-    const solver = window.solver
-
-    model = {
-    "optimize": "capacity",
-    "opType": "max",
-    "constraints": {
-        "plane": {"max": 44},
-        "person": {"max": 512},
-        "cost": {"max": 300000}
-    },
-    "variables": {
-        "brit": {
-            "capacity": 20000,
-            "plane": 1,
-            "person": 8,
-            "cost": 5000
-        },
-        "yank": {
-            "capacity": 30000,
-            "plane": 1,
-            "person": 16,
-            "cost": 9000
-        }
-    },
+function getInputsImob() {
+    var dict = {
+        valor: Number(document.getElementById("valorImovel").value),
+        entrada: Number(document.getElementById("entradaImovel").value),
+        n: Number(document.getElementById("nImovel").value),
+        i_banco: Number(document.getElementById("bancoJurosImovel").value),
+        dorms: Number(document.getElementById("nDormitorios").value)
     };
 
-    results = solver.Solve(model);
-    console.log(results);
+    return dict
 }
+
+function finImob(tempo_compra) {
+    inpts = getInputsImob();
+    let dict = {}
+
+    // coisas q ainda vai mudar
+    tx_juros_mensal = 0.0075
+    alupreco = 0.0037
+    igpm = 0.03
+    renda_fixa = 0.11
+
+    dict["aluguel"] = inpts["valor"] * alupreco;
+    dict["banco"] = inpts["valor"] - inpts["valor"] * inpts["entrada"];
+    dict["parcela"] = dict["banco"] * (tx_juros_mensal / (1 - Math.pow((1 + tx_juros_mensal), (-inpts["n"]*12))));
+    dict["valorTotalFin"] = dict["parcela"] * inpts["n"] * 12;
+    dict["finMinusAlug"] = dict["parcela"] - dict["aluguel"];
+
+    dict["roi"] = (dict["finMinusAlug"]*12) * (Math.pow((1 + renda_fixa), (tempo_compra - 1)) - 1) / renda_fixa;
+
+    dict["valorFinalImov"] = inpts["valor"] * Math.pow((1 + igpm), tempo_compra);
+
+    document.getElementById("finImovel").innerHTML = `O valor final do imóvel é ${dict['valorFinalImov']}`;
+    console.log("TEMPO", tempo_compra)
+    console.log(dict)
+
+    if (Math.round(dict["roi"], 0) != Math.round(dict["valorFinalImov"], 0)){
+        console.log('THIS ONE FAILED')
+        return Infinity
+    } else {
+        console.log("SUCCESS???")
+        return dict["roi"]
+    }
+}
+
+function opt() {
+    optimjs = window.optimjs
+    optimjs.minimize_Powell()
+}
+
+// x^2 + 10x (min x=-5, y=-25)
+
 // function getJSONP(url, success) {
 
 //     var ud = '_' + +new Date,
